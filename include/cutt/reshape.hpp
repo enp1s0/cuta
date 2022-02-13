5d640bc174717f3d6e82c87741c621920fac24ad
mutsuki <mutsuki@momo86.net>
2022-02-13 23:56:33 +0900

    Fix the arguments of `cutt::reshape`

--- include/cutt/reshape.hpp
+++ include/cutt/reshape.hpp
@@ -2,16 +2,17 @@
 #define __CUTT_RESHAPE_HPP__
 #include <cstdint>
 #include <vector>
-#include <map>
+#include <unordered_map>
 
 namespace cutt {
 template <class T>
 void reshape(
 		T* const dst_ptr,
-		const T* src_ptr,
-		const std::map<std::string, unsigned>& mode,
-		const std::vector<std::string> original_order,
-		const std::vector<std::string> reshaped_order
+		const T* const src_ptr,
+		const std::unordered_map<std::string, std::size_t>& mode,
+		const std::vector<std::string>& original_order,
+		const std::vector<std::string>& reshaped_order,
+		cudaStream_t cuda_stream = 0
 		);
 } // namespace cutt
 #endif
