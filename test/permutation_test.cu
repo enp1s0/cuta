417d546bcd14ae8e987dd7711e584f54c970247a
mutsuki <mutsuki@momo86.net>
2022-03-25 21:35:18 +0900

    Add a test of `cuta::utils::get_permutated_mode`

--- test/permutation_test.cu
+++ test/permutation_test.cu
@@ -10,9 +10,9 @@ int main() {
 	cuta::utils::insert_mode(mode, "d", 10);
 	cuta::utils::insert_mode(mode, "e", 10);
 
-	{
-		std::vector<std::string> perm_order = {"b", "a", "e", "d", "c"};
+	std::vector<std::string> perm_order = {"b", "a", "e", "d", "c"};
 
+	{
 		const auto permutation = cuta::utils::get_permutation<>(mode, perm_order);
 
 		std::printf("permutation = ");
@@ -21,5 +21,9 @@ int main() {
 		}
 		std::printf("\n");
 		std::printf("expected    = 1 0 4 3 2\n");
+	}
+	{
+		const auto permutated_mode = cuta::utils::get_permutated_mode(mode, perm_order);
+		cuta::utils::print_mode(permutated_mode);
 	}
 }
