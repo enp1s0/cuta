4b2868df057d13bb5dfcb66e0bf9cff2f7b03f27
mutsuki <mutsuki@momo86.net>
2022-03-25 17:30:43 +0900

    Add a test for permutation

---
+++ test/permutation_test.cu
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cuta/reshape.hpp>
+
+
+int main() {
+	cuta::mode_t mode;
+	cuta::utils::insert_mode(mode, "a", 10);
+	cuta::utils::insert_mode(mode, "b", 10);
+	cuta::utils::insert_mode(mode, "c", 10);
+	cuta::utils::insert_mode(mode, "d", 10);
+	cuta::utils::insert_mode(mode, "e", 10);
+
+	{
+		std::vector<std::string> perm_order = {"b", "a", "e", "d", "c"};
+
+		const auto permutation = cuta::get_permutation(mode, perm_order);
+
+		std::printf("permutation = ");
+		for (const auto& v : permutation) {
+			std::printf("%u ", v);
+		}
+		std::printf("\n");
+		std::printf("expected    = 1 0 4 3 2\n");
+	}
+}
